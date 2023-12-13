15fd860ea16db0c3e9c927c363088d2b7614bff6
pankaj1405 <98148058+pankaj1405@users.noreply.github.com>
2023-12-13 17:19:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
 const express = require('express');
-const axios = require('axios');
-
-const clientId = '39673c85-309e-431c-b81d-25476d8797f8';
-const tenantId = 'add67cd2-c8b2-416c-b171-b61b22be92f4';
-const clientSecret = 'Ank8Q~Y15OoCrKBVONL6m6t4FnICBWlIxnpFtcCo';
-const scope = 'api://6e0063e1-5b74-4730-99c6-2b1541144e5c/.default'; // Access scope of WebApp2 API
+const axios = require('axios');  // Use 'axios' directly
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World! sender...');
 });
 
-app.get('/generate-token', async (req, res) => {
-  try {
-    const body = {
-      grant_type: 'client_credentials',
-      client_id: clientId,
-      client_secret: clientSecret,
-      scope,
-    };
 
-    const response = await axios.post('https://login.microsoftonline.com/add67cd2-c8b2-416c-b171-b61b22be92f4/oauth2/v2.0/token', body);
+// Azure AD configuration for sender
+const azureAdConfig = {
+    tenantId: 'add67cd2-c8b2-416c-b171-b61b22be92f4',
+    clientId: '39673c85-309e-431c-b81d-25476d8797f8',
+    clientSecret: 'Ank8Q~Y15OoCrKBVONL6m6t4FnICBWlIxnpFtcCo',
+   // receiverApiUrl: 'api://6e0063e1-5b74-4730-99c6-2b1541144e5c'
+    //receiverApiUrl: 'https://add67cd2-c8b2-416c-b171-b61b22be92f4.onmicrosoft.com/api://6e0063e1-5b74-4730-99c6-2b1541144e5c',
+};
 
-    const token = response.data.access_token;
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error generating token' });
-  }
+// Request an access token from Azure AD
+async function getAccessToken() {
+    try {
+        const response = await axios.post(`https://login.microsoftonline.com/${azureAdConfig.tenantId}/oauth2/v2.0/token`, {
+            grant_type: 'client_credentials',
+            client_id: azureAdConfig.clientId,
+            client_secret: azureAdConfig.clientSecret,
+            // scope: azureAdConfig.receiverApiUrl + '/.default',
+            scope: 'api://6e0063e1-5b74-4730-99c6-2b1541144e5c/.default',
+
+        });
+        const accessToken = response.data.access_token;
+        console.log('Access Token:', accessToken);
+
+        return response.data.access_token;
+    } catch (error) {
+        console.error('Error getting access token:', error.message);
+        throw error;
+    }
+}
+
+// Handle requests to send data
+app.get('/send-data', async (req, res) => {
+    try {
+        const accessToken = await getAccessToken();
+
+        // Make a request to the receiver with the obtained access token
+        const response = await axios.get(azureAdConfig.receiverApiUrl, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error sending data:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-app.get('/api-call', async (req, res) => {
-  const token = req.headers.authorization.split(' ')[1]; // Extract token from header
-
-  if (!token) {
-    return res.status(401).json({ message: 'Missing authorization token' });
-  }
-
-  try {
-    const response = await axios.get('https://YOUR_WEBAPP2_API_URL', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error calling API' });
-  }
+// Start the sender app
+app.listen(port, () => {
+    console.log(`Sender app listening at http://localhost:${port}`);
 });
-
-app.listen(3000, () => console.log('WebApp1 listening on port 3000'));
